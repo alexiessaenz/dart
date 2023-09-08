071fc315a1326d0854cc04ad502c791e0209204a
alexiessaenz <00400815@uca.edu.sv>
2023-09-08 12:31:56 -0600

    feat: exam with mixins_enums

--- session04/bin/examen_intro_dart.dart
+++ session04/bin/examen_intro_dart.dart
@@ -1,46 +1,119 @@
-class Tienda {
-    String nombre;
-    String direccion;
+mixin Disponible {
+  int disponible= 0;
+}
+mixin Informacion {
+  String nombre= '';
+  String direccion= '';
+}
+enum CategoriaProducto{
+  Lacteos,
+  Carnes,
+  Frutas,
+  Verduras,
+  Panaderia,
+  Abarrotes,
+  Bebidas,
+  Limpieza,
+  Farmacia,
+  Mascotas,
+  Ropa,
+  Juguetes,
+  Ferreteria,
+  Tecnologia,
+  Electrodomesticos,
+  Hogar,
+  Papeleria,
+  Oficina,
+  Libros,
+  Entretenimiento,
+  Otros
+}
+enum DepartamentoProducto{
+  Alimentos,
+  Hogar,
+  Tecnologia,
+  Entretenimiento,
+  Otros
+}
+enum Articulos{
+  Teclado,
+  Mouse,
+  Monitor,
+  CPU,
+  Muebles,
+  Sillon,
+  Mesa,
+  Silla,
+  Cama,
+  Colchon,
+  Refrigerador,
+  Lavadora,
+  Licuadora,
+  Plancha,
+  Television,
+  Radio,
+  Celular,
+  Laptop,
+  Tablet,
+}
+
+class Tienda with Informacion {
     List<Departamento> departamentos;
-    Tienda(this.nombre, this.direccion, this.departamentos);
+    Tienda( this.departamentos);
 }
 
-class Departamento {
-    String nombre;
+class Departamento  {
+    DepartamentoProducto nombre;
     List<Categoria> categorias;
-    Departamento(this.nombre, this.categorias);
+    Departamento({required this.nombre, required this.categorias});
 }
 
-class Categoria {
-  String nombre;
+class Categoria{
+CategoriaProducto nombre;
   List<Articulo> articulos;
-  Categoria(this.nombre, this.articulos);
+  Categoria( {required this.nombre, required this.articulos});
 }
 
-class Articulo {
-    String nombre;
-    int disponible;
-    Articulo(this.nombre,this.disponible);
+class Articulo with Disponible {
+    Articulos nombre;
+    Articulo({required this.nombre});
 }
 
 void main(List<String> args) {
   List<Articulo> articulos = [];
+  List<Articulo> articulos2 = [];
+  List<Categoria> categorias = [];
+  List<Departamento> departamentos = [];
 
-  final articulo1 = Articulo('Computadora', 12);
-  final articulo2 = Articulo('Mouse', 20);
+  final articulo1 = Articulo(nombre: Articulos.Teclado);
+  articulo1.disponible = 10;
+  final articulo2 = Articulo(nombre: Articulos.Mouse);
+  articulo2.disponible = 20;
 
   articulos.add(articulo1);
   articulos.add(articulo2);
 
-  final categoria1 = Categoria('Computacion', articulos);
-  List<Categoria> categorias = [];
+  final articulo3 = Articulo(nombre: Articulos.Muebles);
+  articulos2.add(articulo3);
+  final categoria1 = Categoria(nombre: CategoriaProducto.Tecnologia, articulos: articulos);
   categorias.add(categoria1);
 
-  final departamento1 = Departamento('Departamento de informatica', categorias);
-  List<Departamento> departamentos = [];
+  final categoria2 = Categoria(nombre: CategoriaProducto.Hogar, articulos: articulos2);
+  categorias.add(categoria2);
+
+
+  final departamento1 = Departamento(nombre: DepartamentoProducto.Tecnologia,  categorias: categorias);
   departamentos.add(departamento1);
 
-  final tienda1 = Tienda('Tienda de computadoras', 'Av. 1', departamentos);
+  final departamento2 = Departamento(nombre: DepartamentoProducto.Hogar,  categorias: categorias);
+  departamentos.add(departamento2);
+
+  final tienda1 = Tienda( departamentos);
+  tienda1.nombre = 'Tienda de la esquina';
+  tienda1.direccion = 'Calle 1 # 2-3';
+
+
+  //---------- Imprimir datos de la tienda -----------
   print(tienda1.nombre);
   print(tienda1.direccion);
   for (Departamento departamento in tienda1.departamentos) {
