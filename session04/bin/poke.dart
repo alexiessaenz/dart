a26713f7473d03a513d3a84ceb2511d1bf67d2c8
alexiessaenz <00400815@uca.edu.sv>
2023-09-08 18:51:08 -0600

    upd: pokev1

--- session04/bin/poke.dart
+++ session04/bin/poke.dart
@@ -14,16 +14,56 @@ void main(List<String> arguments) async {
   }
 }
 Future<String> httpGet(String url) async {
-  var request = http.Request('GET', Uri.parse('https://pokeapi.co/api/v2/pokemon/1'));
-
-
+  var request = http.Request('GET', Uri.parse('https://pokeapi.co/api/v2/pokemon/pikachu'));
   http.StreamedResponse response = await request.send();
 
   if (response.statusCode == 200) {
-    print(await (response)=>{
-      var json = response;
-        var scores = jsonDecode(response.stream.bytesToString()));
-    });
+    var str = await response.stream.bytesToString();
+    final Map<String, dynamic> pokemonmap = json.decode(str);
+    dynamic pokemon = json.decode(str);
+    print(pokemon['name']);
+
+    print('type(s): ');
+    pokemon['types'].forEach((k) => print('\t\t' + k['type']['name']));
+
+    print('abilities: ');
+    pokemon['abilities'].forEach((k) => print('\t\t' + k['ability']['name']));
+
+    print('moves: ');
+    pokemon['moves'].forEach( (k) => print('\t\t'+k['move']['name']) );
+    //  var typesj = json.encode(pokemon['types']);
+    // final Map<String, dynamic> types = json.decode(typesj);
+    // print(types);
+    // types.forEach((k,v) => {
+    //   print('----------- $k -----------'),
+    //   print('$k: $v'),
+    //   print('----------- $k -----------\n\n\n')
+    // });
+    // print(pokemon['abilities']);
+    //  pokemon['abilities'].forEach((k,v) => {
+    //   print('----------- $k -----------'),
+    //    print('$k: $v'),
+    //    // k.forEach((k,v) => {
+    //    //   print('----------- $k -----------'),
+    //    //   print('$k: '),
+    //    //   print('----------- $k -----------\n\n\n')
+    //    // }),
+    //   print('-----------   -----------\n\n\n')} );
+    // print(pokemon['name']);
+    // pokemon['species'].forEach((k,v) => {
+    //   print('----------- $k -----------'),
+    //   print('$k: $v'),
+    //   print('----------- $k -----------\n\n\n')
+    // });
+    //
+    // print(pokemon.toString());
+    // pokemon
+
+    // strt.forEach((k,v) => {
+    //   print('----------- $k -----------'),
+    //   print('$k: $v'),
+    //   print('----------- $k -----------\n\n\n')
+    // });
   }
   else {
     print(response.reasonPhrase);
