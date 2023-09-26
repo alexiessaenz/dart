3cf923479c57a692fb49be4e286945ce2e76481b
alexiessaenz <00400815@uca.edu.sv>
2023-09-25 21:21:45 -0600

    files: chat view

--- pokemagic/pokemagic/lib/main.dart
+++ pokemagic/pokemagic/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pokemagic/utils/Poke.dart' as poke;
 poke.Poke pk = new poke.Poke();
 String pkText = '';
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PokeMAgic',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -102,6 +104,17 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+        actions: [
+          IconButton(
+            splashColor: Colors.transparent,
+            icon: const Icon(Icons.search),
+            tooltip: 'Increment',
+            onPressed: (){
+            showSearch(context: context,
+                delegate: MySearchDelegate());
+
+            },
+          ),],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -143,6 +156,63 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.ac_unit),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text('');
+  }
+
+  ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = theme.colorScheme;
+    return theme.copyWith(
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle: colorScheme.brightness == Brightness.dark
+            ? SystemUiOverlayStyle.light
+            : SystemUiOverlayStyle.dark,
+        backgroundColor: colorScheme.brightness == Brightness.dark ? Colors
+            .grey[900] : Theme.of(context).colorScheme.inversePrimary,
+        iconTheme: theme.primaryIconTheme.copyWith(color: colorScheme.secondary),
+        titleTextStyle: theme.textTheme.titleLarge,
+        toolbarTextStyle: theme.textTheme.bodyMedium,
+      ),
+      inputDecorationTheme: searchFieldDecorationTheme ??
+          InputDecorationTheme(
+            hintStyle: searchFieldStyle ?? theme.inputDecorationTheme.hintStyle,
+            border: InputBorder.none,
+          ),
     );
   }
 }
