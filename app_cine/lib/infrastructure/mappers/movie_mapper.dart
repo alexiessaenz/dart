b3767a2e7bc9352e39a6792ab5a56bdfbf5ddc51
alexiessaenz <00400815@uca.edu.sv>
2023-10-13 12:18:26 -0600

    fix: environment vl

--- app_cine/lib/infrastructure/mappers/movie_mapper.dart
+++ app_cine/lib/infrastructure/mappers/movie_mapper.dart
@@ -7,7 +7,7 @@ class MovieMapper {
       backdropPath: (moviedb.backdropPath != '') 
         ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
         : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
-      genreIds: moviedb.genreIds.map( (e) => e.toString() ).toList(),
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
       originalLanguage: moviedb.originalLanguage,
       originalTitle: moviedb.originalTitle,
--- app_cine/lib/main.dart
+++ app_cine/lib/main.dart
@@ -1,29 +1,39 @@
-import 'package:app_cine/config/router/app_router.dart';
-import 'package:app_cine/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+
+import 'package:app_cine/config/router/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:app_cine/config/theme/app_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async {
+Future<void> main() async{
+
   await dotenv.load(fileName: '.env');
+
   runApp(
-      const ProviderScope(
-        child: MainApp())
-    );
+    const ProviderScope(child: MainApp() )
+  );
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: AppTheme().getTheme(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
     );
   }
 }
-
